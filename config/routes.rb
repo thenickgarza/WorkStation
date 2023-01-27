66eb7a3c83bc6b925ad348e2e570bd2027062627
Nick Garza <thenickgarza14@gmail.com>
2023-01-26 21:22:40 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  get "signup" => "users#new"
+  get "login" => "sessions#new"
+  delete "logout" => "sessions#destroy"
+  resources :users, except: [:index, :new]
+  resources :sessions, except:[:index, :edit, :update, :show, :new, :destroy]
+  root "people#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+   root "home#index"
 end
