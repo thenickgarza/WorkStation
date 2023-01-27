f6cb54f92358bbef7f9a438a22c3e6ae80560570
Nick Garza <thenickgarza14@gmail.com>
2023-01-27 01:11:20 -0600

    helper method set_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,16 @@
 class ApplicationController < ActionController::Base
  
-  # Make the current_user method available to views also, not just controllers:
   helper_method :current_user
 
-  helper_method :set_user
+  helper_method :set_user 
 
   def set_user 
     @user = User.find(params[:user_id])
   end
   
-  # Define the current_user method:
+
   def current_user
 
-    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
     if session[:user_id]
       User.find session[:user_id]
     end
