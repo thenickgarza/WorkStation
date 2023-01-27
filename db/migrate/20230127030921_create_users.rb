98a0dea729bdff1c0afc38fec49cb4ad9a08d270
Nick Garza <thenickgarza14@gmail.com>
2023-01-26 21:13:13 -0600

    added users to db

---
+++ db/migrate/20230127030921_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+
+      t.string :first_name, null: false
+      t.string :last_name
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.integer :phone_number
+
+
+      t.timestamps
+    end
+  end
+end
