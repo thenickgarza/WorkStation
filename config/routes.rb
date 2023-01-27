ab4133fe2f72615634542085a6a959132d674eef
Nick Garza <thenickgarza14@gmail.com>
2023-01-26 23:44:12 -0600

    routes -temp

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get "signup" => "users#new"
   get "login" => "sessions#new"
   delete "logout" => "sessions#destroy"
-  resources :users, except: [:index, :new]
+  resources :users  
+    resources :comments
+  
   resources :sessions, except:[:index, :edit, :update, :show, :new, :destroy]
+  
 
    root "home#index"
 end
