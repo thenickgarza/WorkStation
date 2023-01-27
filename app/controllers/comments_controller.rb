d237e2515841e22956961e1972928d06235e4550
Nick Garza <thenickgarza14@gmail.com>
2023-01-26 23:42:59 -0600

    comments controller -temp

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,53 @@
+class CommentsController < ApplicationController
+
+    def index
+       
+        @comment = Comment.all
+    
+    end
+
+    def new
+       
+        @comment = Comment.new
+    
+    end
+
+    def create
+        
+        @comment = Comment.create(comment_params)
+            respond_to do |format|
+                if @comment.save
+                    format.html { redirect_to root_path, notice: "Comment was successfully created. "}
+                    format.json { render :show, status: :created, location: @comment }
+                else
+                    format.html { render :new, status: :unprocessable_entity }
+                    format.json { render @comment.errors, status: :unprocessable_entity }
+                end
+            end
+     end
+
+    def show
+
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+      end 
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private 
+
+    def comment_params
+        
+        params.require(:user).permit(:title, :content)
+    
+    end
+
+end
