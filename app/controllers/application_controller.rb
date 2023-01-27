5ec3ee20c522d0c44066afb32d2be55f64903942
Nick Garza <thenickgarza14@gmail.com>
2023-01-26 21:21:17 -0600

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+ 
+  # Make the current_user method available to views also, not just controllers:
+  helper_method :current_user
+  
+  # Define the current_user method:
+  def current_user
+
+    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      User.find session[:user_id]
+    end
+  end
+
+  def authenticate 
+    if !current_user
+      redirect_to signup_path, alert: "Invalid request"
+    end
+  end
+
+  private
+   
+  def login(user)
+      session[:user_id] = user.id
+    end
+
+    def logout
+      session[:user_id] = nil
+    end 
 end
