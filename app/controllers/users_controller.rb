a9c81a81c489372c881515d74c648f2726a48872
Nick Garza <thenickgarza14@gmail.com>
2023-01-26 21:21:43 -0600

    users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+    before_action :authenticate, except: [:new, :create]
+  
+    def new
+      @user = User.new
+    end
+  
+    def create 
+      @user = User.new user_params
+      if @user.save
+        login(@user)
+        redirect_to root_path, notice: "You have successfully signed up."
+      else
+        render :new, status: :unprocessable_entity
+      end
+     end
+  
+     private
+  
+     def user_params
+      params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, :email)
+     end
+  
+  end
